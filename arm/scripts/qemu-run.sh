8cc50d877ae91ab16ea89fc6f386a297c89cc11f
Will Dicharry <wdicharry@gmail.com>
2018-04-15 14:30:03 -0600

    Run qemu from root.

--- arm/scripts/qemu-run.sh
+++ arm/scripts/qemu-run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+BUILDPATH=./build
+
 QEMU_SYSTEM=${QEMU_SYSTEM:-qemu-system-arm}
 
 MACHINE=raspi2
 #MACHINE=versatilepb
 
-exec ${QEMU_SYSTEM} -M ${MACHINE} -bios kernel.img -nographic
+exec ${QEMU_SYSTEM} -M ${MACHINE} -bios "$BUILDPATH"/src/kernel/kernel.img -nographic
