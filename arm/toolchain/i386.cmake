1f019021bbb55479df35823914a6b5654a780940
Will Dicharry <wdicharry@gmail.com>
2018-05-06 08:47:36 -0600

    Add simple cmake build of x86 assembly.

---
+++ arm/toolchain/i386.cmake
@@ -0,0 +1,9 @@
+set( CMAKE_SYSTEM_NAME Generic )
+set( CMAKE_SYSTEM_PROCESSOR i386 )
+
+set( CMAKE_C_COMPILER gcc )
+set( CMAKE_CXX_COMPILER g++ )
+set( CMAKE_ASM_NASM_COMPILER nasm )
+
+# set( CMAKE_ASM_NASM_FLAGS "-felf32" CACHE STRING "Required NASM flags." )
+set( CMAKE_C_FLAGS "-m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs" CACHE STRING "C Flags" )
