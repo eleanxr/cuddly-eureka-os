63f9fe7980362b06ac27872d0374e268441fe75d
Will Dicharry <wdicharry@gmail.com>
2018-05-19 11:02:00 -0600

    Update build for i386.

--- arm/toolchain/i386.cmake
+++ arm/toolchain/i386.cmake
@@ -5,5 +5,6 @@ set( CMAKE_C_COMPILER gcc )
 set( CMAKE_CXX_COMPILER g++ )
 set( CMAKE_ASM_NASM_COMPILER nasm )
 
-# set( CMAKE_ASM_NASM_FLAGS "-felf32" CACHE STRING "Required NASM flags." )
+set( CMAKE_ASM_NASM_FLAGS "-felf32" CACHE STRING "Required NASM flags." )
 set( CMAKE_C_FLAGS "-m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs" CACHE STRING "C Flags" )
+set( CMAKE_EXE_LINKER_FLAGS "-felf_i386" )
